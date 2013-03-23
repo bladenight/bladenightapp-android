959cb59cc9b179a8e5cbce5cbea742fe993a5411
Olivier Croquette <ocroquette@free.fr>
2013-03-23 21:59:20 +0100

    Fixed build errors contained in the merge

--- src/de/greencity/bladenightapp/android/statistics/StatisticsActivity.java
+++ src/de/greencity/bladenightapp/android/statistics/StatisticsActivity.java
@@ -3,24 +3,21 @@ package de.greencity.bladenightapp.android.statistics;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
-
-import com.markupartist.android.widget.ActionBar;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import de.greencity.bladenightapp.android.R;
-import de.greencity.bladenightapp.android.actionbar.ActionBarConfigurator;
 
 public class StatisticsActivity extends Activity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-    setContentView(R.layout.activity_statistics);
-    getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
-    ImageView titlebar = (ImageView)(findViewById(R.id.icon));
-    titlebar.setImageResource(R.drawable.ic_stats);
-    TextView titletext = (TextView)findViewById(R.id.title);
-    titletext.setText(R.string.title_statistics);
-  }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		setContentView(R.layout.activity_statistics);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+		ImageView titlebar = (ImageView)(findViewById(R.id.icon));
+		titlebar.setImageResource(R.drawable.ic_stats);
+		TextView titletext = (TextView)findViewById(R.id.title);
+		titletext.setText(R.string.title_statistics);
+	}
 } 
