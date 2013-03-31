1250369ef970604e113f48a1e45100dd77d74545
Benjamin Uekermann <benjamin.uekermann@gmail.com>
2013-03-31 22:08:21 +0200

    new action addFriend

---
+++ src/de/greencity/bladenightapp/android/actionbar/ActionAddFriend.java
@@ -0,0 +1,24 @@
+package de.greencity.bladenightapp.android.actionbar;
+
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+
+import com.markupartist.android.widget.ActionBar.Action;
+
+import de.greencity.bladenightapp.android.R;
+import de.greencity.bladenightapp.android.social.AddFriendDialog;
+import de.greencity.bladenightapp.android.social.SocialActivity;
+
+public class ActionAddFriend implements Action {
+    @Override
+    public int getDrawable() {
+        return R.drawable.ic_action_users;
+    }
+
+	@Override
+	public void performAction(View view) {
+	    FragmentManager fm = ((SocialActivity) view.getContext()).getSupportFragmentManager();
+	    AddFriendDialog addFriendDialog = new AddFriendDialog();
+		addFriendDialog.show(fm, "fragment_add_friend");
+	}
+}
