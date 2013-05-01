5d38ec784ad9969427f627c6e3c49d4cf4642730
Olivier Croquette <ocroquette@free.fr>
2013-05-01 18:24:28 +0200

    List to both and GPS_PROVIDER and NETWORK_PROVIDER but keep the best coordinates

--- src/de/greencity/bladenightapp/android/tracker/BladenightLocationListener.java
+++ src/de/greencity/bladenightapp/android/tracker/BladenightLocationListener.java
@@ -13,11 +13,18 @@ public class BladenightLocationListener implements LocationListener {
 	}
 
 	@Override
-	public void onLocationChanged(Location location) {
-		Log.d(this.toString(), "onLocationChanged" + location);
-		this.location.setLatitude(location.getLatitude());
-		this.location.setLongitude(location.getLongitude());
-		this.location.setAccuracy(location.getAccuracy());
+	public void onLocationChanged(Location newLocation) {
+		Log.d(this.toString(), "onLocationChanged" + newLocation);
+		if ( ! isBetterLocation(newLocation, location) ) {
+			Log.i(TAG, "New position is not better, ignoring it");
+			Log.i(TAG, "new location ="+location);
+			Log.i(TAG, "current location ="+newLocation);
+		}
+			
+		location.setLatitude(newLocation.getLatitude());
+		location.setLongitude(newLocation.getLongitude());
+		location.setAltitude(newLocation.getAltitude());
+		location.setAccuracy(newLocation.getAccuracy());
 	}
 
 	@Override
@@ -35,5 +42,63 @@ public class BladenightLocationListener implements LocationListener {
 		Log.d(this.toString(), "onStatusChanged: " + provider + " status="+status);
 	}
 
+	/** Determines whether one Location reading is better than the current Location fix
+	  * @param location  The new Location that you want to evaluate
+	  * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	  */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+	    if (currentBestLocation == null) {
+	        // A new location is always better than no location
+	        return true;
+	    }
+
+	    // Check whether the new location fix is newer or older
+	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+	    boolean isSignificantlyNewer = timeDelta > MAX_DELTA_TIME;
+	    boolean isSignificantlyOlder = timeDelta < -MAX_DELTA_TIME;
+	    boolean isNewer = timeDelta > 0;
+
+	    // If it's been a long time since the current location, use the new location
+	    // because the user has likely moved
+	    if (isSignificantlyNewer) {
+	        return true;
+	    // If the new location is more than two minutes older, it must be worse
+	    } else if (isSignificantlyOlder) {
+	        return false;
+	    }
+
+	    // Check whether the new location fix is more or less accurate
+	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+	    boolean isLessAccurate = accuracyDelta > 0;
+	    boolean isMoreAccurate = accuracyDelta < 0;
+	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+	    // Check if the old and new location are from the same provider
+	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+	            currentBestLocation.getProvider());
+
+	    // Determine location quality using a combination of timeliness and accuracy
+	    if (isMoreAccurate) {
+	        return true;
+	    } else if (isNewer && !isLessAccurate) {
+	        return true;
+	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+	    if (provider1 == null) {
+	      return provider2 == null;
+	    }
+	    return provider1.equals(provider2);
+	}
+	
+
 	private Location location;
+	private static final int MAX_DELTA_TIME = 1000 * 60 * 2;
+	private static String TAG = "BladenightLocationListener";
+
 }
