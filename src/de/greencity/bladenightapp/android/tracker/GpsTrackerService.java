961266a872701c9ca78f74187229a1c5311f9670
Olivier Croquette <ocroquette@free.fr>
2013-08-13 23:12:51 -0400

    Notification icon has to be scalled under Android 4.x With the API level 11 or greated, the required size is available

--- src/de/greencity/bladenightapp/android/tracker/GpsTrackerService.java
+++ src/de/greencity/bladenightapp/android/tracker/GpsTrackerService.java
@@ -3,10 +3,14 @@ package de.greencity.bladenightapp.android.tracker;
 import java.io.File;
 import java.io.IOException;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.os.Binder;
 import android.os.Handler;
@@ -33,9 +37,9 @@ public class GpsTrackerService extends Service {
 		gpsListener = new GpsListener(this, locationListener);
 
 		gpsListener.requestLocationUpdates(5000);
-		
+
 		traceLogger = new GeoTraceLogger(new File(Paths.getAppDataDirectory(), "gps-trace.txt"));
-		
+
 		realTimeDataConsumer = new RealTimeDataConsumer() {
 			@Override
 			public void consume(RealTimeUpdateData realTimeUpdateData) {
@@ -47,7 +51,7 @@ public class GpsTrackerService extends Service {
 				writeTraceEntry();
 			}
 		};
-		
+
 		networkClient.addRealTimeDataConsumer(realTimeDataConsumer);
 
 		setNotification();
@@ -96,16 +100,35 @@ public class GpsTrackerService extends Service {
 		Intent notificationIntent = new Intent(this, BladenightMapActivity.class);
 		PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
+		Bitmap icon = getNotificationIcon();
+		Log.i(TAG, icon.toString());
 		Notification notification = new  NotificationCompat.Builder(this)
 		.setContentTitle(getString(R.string.msg_tracking_running))
 		.setContentText(getString(R.string.app_name))
 		.setSmallIcon(R.drawable.application_prod)
+		.setLargeIcon(getNotificationIcon())
 		.setContentIntent(contentIntent)
 		.build();
-		
+
 		notification.flags |= Notification.FLAG_FOREGROUND_SERVICE;
 
 		startForeground(NOTIFICATION_ID, notification);
+	}
+
+	@SuppressLint("InlinedApi")
+	private Bitmap getNotificationIcon() {
+		Bitmap rawBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.application_prod);
+
+		if (android.os.Build.VERSION.SDK_INT < 11) {
+			return rawBitmap;
+		}
+		else {
+			Resources res = getResources();
+			int height = (int) res.getDimension(android.R.dimen.notification_large_icon_height);
+			int width = (int) res.getDimension(android.R.dimen.notification_large_icon_width);
+			
+			return Bitmap.createScaledBitmap(rawBitmap, width, height, false);
+		}
 	}
 
 	private void sendLocationUpdateToNetworkService() {
@@ -132,12 +155,12 @@ public class GpsTrackerService extends Service {
 	private Runnable periodicNetworkSenderRunnable;
 	private RealTimeDataConsumer realTimeDataConsumer;
 	private GeoTraceLogger traceLogger;
-	
+
 	final Handler handler = new Handler();
 	static private final int SEND_PERIOD = 10000;
 	static private final int MIN_LOG_PERIOD = 2000;
 	static private final int NOTIFICATION_ID = 1;
-	
+
 	static final String TAG = "GpsTrackerService";
 	static final String INTENT_PERIODIC = "de.greencity.bladenightapp.android.gps.periodic";
 }
