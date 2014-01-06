9699e934d6ab8773f82000050d1c1a9ed736279d
Benjamin Uekermann <benjamin.uekermann@gmail.com>
2014-01-06 21:13:39 +0100

    added ugly dummy chart

--- src/de/greencity/bladenightapp/android/statistics/StatisticsActivity.java
+++ src/de/greencity/bladenightapp/android/statistics/StatisticsActivity.java
@@ -4,6 +4,12 @@ package de.greencity.bladenightapp.android.statistics;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 import org.joda.time.format.DateTimeFormatter;
 
 import android.app.Activity;
@@ -12,6 +18,7 @@ import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Window;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.markupartist.android.widget.ActionBar;
@@ -37,6 +44,16 @@ public class StatisticsActivity extends Activity {
 		super.onResume();
 		// Log.i(TAG, "onResume");
 		configureActionBar();
+		super.onResume();
+        LinearLayout layout = (LinearLayout) findViewById(R.id.chart);
+        if (mChart == null) {
+            initChart();
+            addSampleData();
+            mChart = ChartFactory.getCubeLineChartView(this, mDataset, mRenderer, 0.3f);
+            layout.addView(mChart);
+        } else {
+            mChart.repaint();
+        }
 	}
 	
 	@Override
@@ -103,4 +120,34 @@ public class StatisticsActivity extends Activity {
 	
 	final static String TAG = "StatisticsActivity";
 	public static final String PARAM_EVENT_MESSAGE = "eventMessage";
+	
+	
+	private GraphicalView mChart;
+
+    private XYMultipleSeriesDataset mDataset = new XYMultipleSeriesDataset();
+
+    private XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+
+    private XYSeries mCurrentSeries;
+
+    private XYSeriesRenderer mCurrentRenderer;
+
+    private void initChart() {
+        mCurrentSeries = new XYSeries("Sample Data");
+        mDataset.addSeries(mCurrentSeries);
+        mCurrentRenderer = new XYSeriesRenderer();
+        mRenderer.addSeriesRenderer(mCurrentRenderer);
+    }
+
+    private void addSampleData() {
+        mCurrentSeries.add(1, 2);
+        mCurrentSeries.add(2, 3);
+        mCurrentSeries.add(3, 2);
+        mCurrentSeries.add(4, 5);
+        mCurrentSeries.add(5, 4);
+    }
+
+    
+
+  
 } 
